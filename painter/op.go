f005b3348c8dd2ce182d6781bb0927f5eac33f40
Alex Shopiak <alshop2004@gmail.com>
2023-04-30 22:59:10 +0200

    Update operations

--- painter/op.go
+++ painter/op.go
@@ -8,12 +8,11 @@ import (
 )
 
 //Зберігає поточний стан текстури
-type TextureState struct {
-	BgClr color.Color
-	BgRect Operation
-	Figures []Figure
-}
 var state = TextureState{}
+type TextureState struct {
+	BgRect Operation
+	Figures []FigureOp
+}
 
 //Змінює вхідну текстуру
 //Повертаючи true, якщо текстура готова для відображення
@@ -31,9 +30,8 @@ func (ol OperationList) Do(t screen.Texture) (ready bool) {
 }
 
 //Сигналізує, що текстура готова
-type updateOp struct{}
-func (op updateOp) Do(t screen.Texture) bool { return true }
-var UpdateOp = updateOp{}
+type UpdateOp struct{}
+func (op UpdateOp) Do(t screen.Texture) bool { return true }
 
 //Перетворює функцію оновлення текстури в Operation
 type OperationFunc func(t screen.Texture)
@@ -44,56 +42,53 @@ func (f OperationFunc) Do(t screen.Texture) bool {
 
 //Зафарбовує тестуру у білий колір. Може бути викоистана як Operation через OperationFunc(WhiteFill).
 func WhiteFill(t screen.Texture) {
-	state.BgClr = color.White
-	t.Fill(t.Bounds(), state.BgClr, screen.Src)
+	t.Fill(t.Bounds(), color.White, screen.Src)
 }
 //Зафарбовує тестуру у зелений колір. Може бути викоистана як Operation через OperationFunc(GreenFill).
 func GreenFill(t screen.Texture) {
-	state.BgClr = color.RGBA{G: 0xff, A: 0xff}
-	t.Fill(t.Bounds(), state.BgClr , screen.Src)
+	t.Fill(t.Bounds(), color.RGBA{G: 0xff, A: 0xff} , screen.Src)
+}
+//Очищує текстуру та зафарбовує у чорний колір. Може бути викоистана як Operation через OperationFunc(Reset).
+func Reset(t screen.Texture) {	
+	state.BgRect = nil
+	state.Figures = state.Figures[:0] //TODO can nil be here
+	t.Fill(t.Bounds(), color.Black, screen.Src)
 }
 
 
-type BgRect struct{
-	x1, y1, x2, y2 int 
+
+type BgrectOp struct{
+	X1, Y1, X2, Y2 int 
 }
-func (r *BgRect) Do(t screen.Texture) bool {
+func (r BgrectOp) Do(t screen.Texture) bool {
 	state.BgRect = r
 	c := color.Black
-	t.Fill(image.Rect(r.x1, r.y1, r.x2, r.y2), c, screen.Src)
+	t.Fill(image.Rect(r.X1, r.Y1, r.X2, r.Y2), c, screen.Src) //neeedd
 	return false
 }
 
 
-type Figure struct{
-	x, y int
+
+type FigureOp struct{
+	X, Y int
 }
-func (f *Figure) Do(t screen.Texture) bool {
-	state.Figures = append(state.Figures, *f)
+func (f FigureOp) Do(t screen.Texture) bool {
+	state.Figures = append(state.Figures, f)
 	c := color.RGBA{R: 225, G: 225, B: 0, A: 1}
-	t.Fill(image.Rect(f.x-200, f.y-200, f.x+200, f.y), c, screen.Src)
-	t.Fill(image.Rect(f.x-100, f.y-200, f.x+100, f.y+200), c, screen.Src)
+	t.Fill(image.Rect(f.X-200, f.Y-200, f.X+200, f.Y), c, screen.Src)
+	t.Fill(image.Rect(f.X-100, f.Y, f.X+100, f.Y+200), c, screen.Src)
 	return false 
 }
 
 
-type Move struct{
-	x, y int
+type MoveOp struct{
+	X, Y int
 }
-func (m *Move) Do(t screen.Texture) bool {
+func (m MoveOp) Do(t screen.Texture) bool {
 	for _, f := range state.Figures {
-		f.x += m.x
-		f.y += m.y
+		f.X += m.X
+		f.Y += m.Y
 		//todo
 	}
 	return false
-}
-
-
-func Reset(t screen.Texture) {	
-	state.BgClr = color.Black
-	state.BgRect = nil
-	state.Figures = state.Figures[:0] //TODO can nil be here
-
-	t.Fill(t.Bounds(), state.BgClr, screen.Src)
 }
