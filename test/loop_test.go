da43b9eecb961f46bdf03caf21ab6b37709f1a39
Alex Shopiak <alshop2004@gmail.com>
2023-04-30 11:10:36 +0200

    Update Loop test

--- test/loop_test.go
+++ test/loop_test.go
@@ -4,18 +4,18 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
+	"reflect"
 	"testing"
 
 	"github.com/AlexShopiak/kpi-lab3/painter"
 	"golang.org/x/exp/shiny/screen"
 )
-func TestLoop_Post(t *testing.T) {
+func TestLoop_Post1(t *testing.T) {
 	var (
 		l painter.Loop
 		tr TestReceiver
 	)
 	l.Receiver = &tr
-
 	l.Start(mockScreen{})
 
 	l.Post(painter.OperationFunc(painter.WhiteFill)) //#1
@@ -37,14 +37,63 @@ func TestLoop_Post(t *testing.T) {
 	}
 }
 
-//Mocks
+func TestLoop_Post2(t *testing.T) {
+	var (
+		l painter.Loop
+		tr TestReceiver
+	)
+	l.Receiver = &tr
+	l.Start(mockScreen{})
+
+	l.Post(painter.OperationFunc(painter.GreenFill)) //#1
+	l.Post(painter.UpdateOp)
+	l.Post(painter.OperationFunc(painter.GreenFill)) //#1
+	l.Post(painter.UpdateOp)
+
+	l.StopAndWait()
+
+	tx, _ := tr.LastTexture.(*mockTexture)
+	if tx.FillCnt != 1 {
+		t.Fatal("Unexpected num of Fill calls")
+	}
+}
+
+func TestLoop_Post3(t *testing.T) {
+	var (
+		l painter.Loop
+		tr TestReceiver
+	)
+	l.Receiver = &tr
+	l.Start(mockScreen{})
+	var testOps []string
+
+	l.Post(painter.OperationFunc(func(t screen.Texture) {
+		testOps = append(testOps, "op1")
+
+		l.Post(painter.OperationFunc(func(t screen.Texture) {
+			testOps = append(testOps, "op3")
+		}))
+	}))
+
+	l.Post(painter.OperationFunc(func(t screen.Texture) {
+		testOps = append(testOps, "op2")
+	}))
+
+	l.StopAndWait()
+
+	if !reflect.DeepEqual(testOps, []string{"op1","op2","op3"}) {
+		t.Fatal("Bad prder of operations")
+	}
+}
+
+//=================================Mocks===================================
+//=========================================================================
 type TestReceiver struct {
-	LastTexture screen.Texture	
+	LastTexture screen.Texture
 }
 func (tr *TestReceiver) Update(t screen.Texture) {
 	tr.LastTexture = t
 }
-
 
 
 type mockScreen struct {}
@@ -59,13 +108,16 @@ func (m mockScreen) NewWindow(opts *screen.NewWindowOptions) (screen.Window, err
 }
 
 
-
 type mockTexture struct {
 	FillCnt int
 }
 func (m *mockTexture) Release() {}
-func (m *mockTexture) Size() image.Point { return image.Pt(400, 400) }
-func (m *mockTexture) Bounds() image.Rectangle { return image.Rectangle{Max: image.Pt(400, 400)} }
+func (m *mockTexture) Size() image.Point {
+	return image.Pt(400, 400)
+}
+func (m *mockTexture) Bounds() image.Rectangle {
+	return image.Rectangle{Max: image.Pt(400, 400)}
+}
 func (m *mockTexture) Upload(dp image.Point, src screen.Buffer, sr image.Rectangle) {
 	panic("Implement me")
 }
