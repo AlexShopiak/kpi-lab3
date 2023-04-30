8bbc05ce17a8c03ea18a254b8e1e27503ce19cb5
Alex Shopiak <alshop2004@gmail.com>
2023-04-30 11:15:01 +0200

    Update stub

--- test/parser_test.go
+++ test/parser_test.go
@@ -1,8 +1,12 @@
 package test
 
-import "testing"
-
-func TestStub(t *testing.T) {
+import (
+	"testing"
+	//"github.com/AlexShopiak/kpi-lab3/painter/lang"
+)
+//TODO add tests for parser
+func TestParser_Stub(t *testing.T) {
+	//p := lang.Parser{}
 
 	if "5" == "6" {
 		t.Fatal("Ooops")
